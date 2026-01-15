2b5431dda4bfe627280706a1804d72a4a11a3fd0
samih freire <samihcisne@gmail.com>
2026-01-15 16:24:19 -0300

    feat: criando o endpoint pra salvar despesa

--- src/CashFlow.Api/Program.cs
+++ src/CashFlow.Api/Program.cs
@@ -7,6 +7,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddRouting(option => option.LowercaseUrls = true); // Força todas as urls serem minúsculas
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
