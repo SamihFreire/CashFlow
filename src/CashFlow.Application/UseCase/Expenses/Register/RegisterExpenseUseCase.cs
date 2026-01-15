c6fbc87bdab282db53cb3f6d2f50f83079d1f0c8
samih freire <samihcisne@gmail.com>
2026-01-15 17:27:41 -0300

    feat: criado função de  validação das despesas

--- src/CashFlow.Application/UseCase/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCase/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,4 +1,5 @@
-﻿using CashFlow.Communication.Requests;
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 using System;
 using System.Collections.Generic;
@@ -12,9 +13,27 @@ namespace CashFlow.Application.UseCase.Expenses.Register
     {
         public ResponseRegisteredExpenseJson Execute(RequestRegisterExpenseJson request)
         {
-            // TO DO VALIDATIONS
+            Validate(request);
 
             return new ResponseRegisteredExpenseJson();
+        }
+
+        private void Validate(RequestRegisterExpenseJson request)
+        {
+            var titleIsEmpty = string.IsNullOrWhiteSpace(request.Title);
+            if(titleIsEmpty)
+                throw new ArgumentException("The title is required.");
+
+            if(request.Amount <= 0) 
+                throw new ArgumentException("The Amount must be grater tha zero.");
+
+            var result = DateTime.Compare(request.Date, DateTime.UtcNow);
+            if (result > 0)
+                throw new ArgumentException("Expenses cannot be for the future.");
+
+            var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentType), request.PaymentType);
+            if (!paymentTypeIsValid)
+                throw new ArgumentException("Payment type is not valid.");
         }
     }
 }
