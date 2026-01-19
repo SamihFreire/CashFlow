5240b82fa975d3e0a8d1685a49328cdf65e37b46
samih freire <samihcisne@gmail.com>
2026-01-19 14:49:36 -0300

    tests: adicionado primeiro teste de sucesso

---
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenserValidatorTests.cs
@@ -0,0 +1,35 @@
+﻿using CashFlow.Application.UseCase.Expenses.Register;
+using CashFlow.Communication.Requests;
+
+namespace Validators.Tests.Expenses.Register
+{
+    public class RegisterExpenserValidatorTests
+    {
+        // Com o Fact sinalizamos que se trata de uma função de teste de unidade
+        [Fact]
+        public void Success()
+        {
+            // Arrange: Criação das instancias de tudo que precisamos para executar os testes
+            
+            // Precisamos adicionar a referencia de application para ter acesso ao RegisterExpenseValidator
+            var validator = new RegisterExpenseValidator();
+
+            // Precisamos adicionar a referencia de communication para ter acesso ao RequestRegisterExpenseJson
+            var request = new RequestRegisterExpenseJson
+            {
+                Amount = 100,
+                Date = DateTime.Now.AddDays(-1),
+                Description = "Description",
+                Title = "Apple",
+                PaymentType = CashFlow.Communication.Enums.PaymentType.CreditCard
+            };
+
+            // Act: Ação que queremos testa, no caso testar vamos testar se esta válido os dados da requisição
+            var result = validator.Validate(request);
+
+            // Assert: Compara o resultado obtido com o resultado esperado
+            // Verificando se o result é verdadeiro com a classe Assert
+            Assert.True(result.IsValid);
+        }
+    }
+}
