802d276da0c1b533b2a31428ade2370532b4d4d1
samih freire <samihcisne@gmail.com>
2026-01-19 12:25:14 -0300

    feat: criado validação dos idiomas solicitados no Middleware

--- src/CashFlow.Api/Middleware/CultureMiddleware.cs
+++ src/CashFlow.Api/Middleware/CultureMiddleware.cs
@@ -16,17 +16,23 @@ namespace CashFlow.Api.Middleware
         // O context é usado para podermos acessar a requisição recebida
         public async Task Invoke(HttpContext context)
         {
+            // Buscando todas as linguagens suportadas pelo .net
+            var supportedLanguages = CultureInfo.GetCultures(CultureTypes.AllCultures).ToList();
+
             // Extraindo do Header da requisição a linguagem que aplicação deseja
-            var culture = context.Request.Headers.AcceptLanguage.FirstOrDefault();
+            var requestedCulture = context.Request.Headers.AcceptLanguage.FirstOrDefault();
 
             // Definindo que a linguagem padrão é Inglês
             var cultureInfo = new CultureInfo("en");
             
             // Caso seja especificado a linguagem no header
-            if (!string.IsNullOrWhiteSpace(culture))
+            // E a linguagem solicitada é suportada pelo .net
+            // ele define a linguagem solicitada
+            if (!string.IsNullOrWhiteSpace(requestedCulture) 
+                && supportedLanguages.Exists(language => language.Name.Equals(requestedCulture)))
             {
                 // Passando como parametro a linguagem solicitada
-                cultureInfo = new CultureInfo(culture);
+                cultureInfo = new CultureInfo(requestedCulture);
             }
 
             // Altera a API para devolver a cultura correta
