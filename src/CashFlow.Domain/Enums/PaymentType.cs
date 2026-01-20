97faf83a7f118e9347bcbd00f0027721adb06ac2
samih freire <samihcisne@gmail.com>
2026-01-20 15:15:04 -0300

    feat: adicionado no domain a entidade expense

---
+++ src/CashFlow.Domain/Enums/PaymentType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashFlow.Domain.Enums
+{
+    public enum PaymentType
+    {
+        Cash,
+        CreditCard,
+        DebitCard,
+        EletronicTransfer,
+    }
+}
