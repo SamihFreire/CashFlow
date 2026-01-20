545e3c5e32b3de53ac4041de2ce5536ff54f0330
samih freire <samihcisne@gmail.com>
2026-01-20 17:54:37 -0300

    feat: adicionando e configurando dbcontext

---
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CashFlow.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashFlow.Infrastructure.DataAccess
+{
+    public class CashFlowDbContext : DbContext
+    {
+        public DbSet<Expense> Expenses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var conncetionString = "Server=localhost;Database=cashflowdb;Uid=root;Pwd=@dm1nT1";
+            var serverVersion = new MySqlServerVersion(new Version(8,0,45));
+
+            optionsBuilder.UseMySql(serverVersion);
+        }
+    }
+}
