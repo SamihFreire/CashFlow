4f0beb54867253aeed8029720e6b5fdc7543c0c7
samih freire <samihcisne@gmail.com>
2026-01-21 10:24:32 -0300

    feat: adicionado interface de expense repository

---
+++ src/CashFlow.Domain/Repositories/Expenses/IExpensesRespository.cs
@@ -0,0 +1,9 @@
+﻿using CashFlow.Domain.Entities;
+
+namespace CashFlow.Domain.Repositories.Expenses
+{
+    public interface IExpensesRespository
+    {
+        public void add(Expense expense);
+    }
+}
