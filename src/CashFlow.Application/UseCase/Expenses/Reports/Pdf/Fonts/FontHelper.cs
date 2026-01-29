73884eccb1acf6dd6d2e7dc00b307fd2f4cdb9de
samih freire <samihcisne@gmail.com>
2026-01-29 14:20:04 -0300

    feat: iniciado fonte resolver

---
+++ src/CashFlow.Application/UseCase/Expenses/Reports/Pdf/Fonts/FontHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashFlow.Application.UseCase.Expenses.Reports.Pdf.Fonts
+{
+    public class FontHelper
+    {
+        public const string RALEWAY_REGULAR = "Raleway-Regular";
+        public const string RALEWAY_BLACK = "Raleway-Black";
+        public const string WORKSANS_REGULAR = "WorkSans-Regular";
+        public const string WORKSANS_BLACK = "WorkSans-Black";
+    }
+}
--- src/CashFlow.Application/UseCase/Expenses/Reports/Pdf/GenerateExpenseReportPdfUseCase.cs
+++ src/CashFlow.Application/UseCase/Expenses/Reports/Pdf/GenerateExpenseReportPdfUseCase.cs
@@ -1,4 +1,6 @@
-﻿using CashFlow.Domain.Repositories.Expenses;
+﻿using CashFlow.Application.UseCase.Expenses.Reports.Pdf.Fonts;
+using CashFlow.Domain.Repositories.Expenses;
+using PdfSharp.Fonts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +17,8 @@ namespace CashFlow.Application.UseCase.Expenses.Reports.Pdf
         public GenerateExpenseReportPdfUseCase(IExpensesReadOnlyRepository repository)
         {
             _repository = repository;
+
+            GlobalFontSettings.FontResolver = new ExpensesReportFontResolver();
         }
 
         public async Task<byte[]> Execute(DateOnly month)
