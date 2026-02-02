9e54fd829ffab2d9c767ae7d3c733b383c57960b
samih freire <samihcisne@gmail.com>
2026-02-02 08:49:02 -0300

    feat: adicionando primeira migration

--- src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace CashFlow.Infrastructure.DataAccess
 {
-    internal class CashFlowDbContext : DbContext
+    public class CashFlowDbContext : DbContext
     {
         // : base(options) -> Esta repassando para o contrutor da classe base(o DbContext) as options
         public CashFlowDbContext(DbContextOptions options) : base(options) { }
