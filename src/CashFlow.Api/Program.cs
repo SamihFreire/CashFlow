ed85b683184adf9805593a712daad60e304fa5da
samih freire <samihcisne@gmail.com>
2026-02-03 11:27:56 -0300

    feat: configurado autenticação da api

--- src/CashFlow.Api/Program.cs
+++ src/CashFlow.Api/Program.cs
@@ -3,7 +3,11 @@ using CashFlow.Api.Middleware;
 using CashFlow.Application;
 using CashFlow.Infrastructure;
 using CashFlow.Infrastructure.Migrations;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -28,6 +32,27 @@ builder.Services.AddMvc(options => options.Filters.Add(typeof(ExceptionFilter)))
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
 
+#region indicando pra API que vai ser usado o authentication
+
+var signinKey = builder.Configuration.GetValue<string>("Settings:Jwt:SigningKey");
+
+builder.Services.AddAuthentication(config =>
+{
+    config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(config =>
+{
+    config.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ClockSkew = new TimeSpan(0),
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(signinKey!))
+    };
+});
+
+#endregion
+
 builder.Services.AddRouting(option => option.LowercaseUrls = true); // Forca todas as urls serem minusculas
 
 var app = builder.Build();
@@ -44,6 +69,7 @@ app.UseMiddleware<CultureMiddleware>();
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication(); // Configurando o authentication
 app.UseAuthorization();
 
 app.MapControllers();
