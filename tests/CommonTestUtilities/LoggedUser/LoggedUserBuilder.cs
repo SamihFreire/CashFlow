4bd0e11a980f6b88746608af33f49a145a35875e
SamihFreire <samihcisne@gmail.com>
2026-02-17 15:54:14 -0300

    test: adicionado teste unitário para o fluxo de registrar uma despesa

---
+++ tests/CommonTestUtilities/LoggedUser/LoggedUserBuilder.cs
@@ -0,0 +1,25 @@
+﻿using CashFlow.Domain.Entities;
+using CashFlow.Domain.Services.LoggedUser;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonTestUtilities.LoggedUser
+{
+    public class LoggedUserBuilder
+    {
+        public static ILoggedUser Build(User user)
+        {
+            var mock = new Mock<ILoggedUser>();
+
+            //  O returnAsync com o user passado como parâmetro serve para configurar o comportamento do método Get() do ILoggedUser.
+            // Quando o método Get() for chamado durante os testes, ele retornará o objeto user que foi passado para o método Build.
+            mock.Setup(loggedUser => loggedUser.Get()).ReturnsAsync(user);
+
+            return mock.Object;
+        }
+    }
+}
---
+++ tests/CommonTestUtilities/Repositories/ExpensesWriteOnlyRepositoryBuilder.cs
@@ -0,0 +1,20 @@
+﻿using CashFlow.Domain.Repositories.Expenses;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonTestUtilities.Repositories
+{
+    public class ExpensesWriteOnlyRepositoryBuilder
+    {
+        public static IExpensesWriteOnlyRepository Build()
+        {
+            var mock = new Mock<IExpensesWriteOnlyRepository>();
+
+            return mock.Object;
+        }
+    }
+}
