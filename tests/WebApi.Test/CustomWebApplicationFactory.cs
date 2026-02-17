98f3a763ddd0d88dd19a26c025d33850dec27c19
SamihFreire <samihcisne@gmail.com>
2026-02-17 17:45:53 -0300

    refact: refatorado propriedades dos testes de integração

--- tests/WebApi.Test/CustomWebApplicationFactory.cs
+++ tests/WebApi.Test/CustomWebApplicationFactory.cs
@@ -7,17 +7,16 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using WebApi.Test.Resources;
 
 namespace WebApi.Test
 {
     // CustomWebApplicationFactory é uma classe personalizada que herda de WebApplicationFactory<Program> (disponibiliza um servidor de teste), onde Program é a classe principal da aplicação web. Essa classe é usada para configurar o ambiente de teste e criar um servidor de teste para a aplicação web.
     public class CustomWebApplicationFactory : WebApplicationFactory<Program>
     {
-        private Expense _expense;
-
-        private User _user;
-        private string _password;
-        private string _token;
+        public ExpenseIdentityManager Expense { get; private set; } = default!;
+        public UserIdentityManager User_Team_Member { get; private set; } = default!;
+        public UserIdentityManager User_Admin { get; private set; } = default!;
 
         // ConfigureWebHost é um método que pode ser sobrescrito para configurar o ambiente de teste.
         // Nesse caso, estamos definindo o ambiente como "Test", o que pode ser usado para carregar configurações específicas para testes, como bancos de dados em memória ou outras dependências de teste.
@@ -47,48 +46,55 @@ namespace WebApi.Test
                     // Obtendo uma instância do IPasswordEncripter para encriptar senhas
                     var passwordEncripter = scope.ServiceProvider.GetRequiredService<IPasswordEncripter>();
 
-                    // Iniciando o banco de dados com dados de teste
-                    StartDatabase(dbContext, passwordEncripter);
-                    
-                    // Obtendo uma instância do IAccessTokenGenerator para gerar tokens de acesso
-                    var tokenGenerator = scope.ServiceProvider.GetRequiredService<IAccessTokenGenerator>();
+                    var accessTokenGenerator = scope.ServiceProvider.GetRequiredService<IAccessTokenGenerator>();
 
-                    _token = tokenGenerator.Generate(_user); // Gerando um token de acesso para o usuário de teste criado no banco de dados em memória
+                    // Iniciando o banco de dados com dados de teste
+                    StartDatabase(dbContext, passwordEncripter, accessTokenGenerator);
+                    
                 });
         }
 
-        public string GetName() => _user.Name;
-        public string GetEmail() => _user.Email;
-        public string GetPassword() => _password;
-        public string GetToken() => _token;
-        public long GetExpenseId() => _expense.Id;
-
         // Sempre que um teste for executado, o método StartDataBase será chamado para garantir que o banco de dados em memória seja inicializado com os dados necessários para os testes.
         // Isso é especialmente útil para garantir que os testes sejam consistentes e independentes, já que cada teste pode começar com um estado conhecido do banco de dados.
-        private void StartDatabase(CashFlowDbContext dbContext, IPasswordEncripter passwordEncripter)
+        private void StartDatabase(
+            CashFlowDbContext dbContext, 
+            IPasswordEncripter passwordEncripter, 
+            IAccessTokenGenerator accessTokenGenerator)
         {
-            AddUsers(dbContext, passwordEncripter);
-            AddExpenses(dbContext, _user);
+            var user = AddUsersTeamMember(dbContext, passwordEncripter, accessTokenGenerator);
+            AddExpenses(dbContext, user);
 
             dbContext.SaveChanges();
         }
 
-        private void AddUsers(CashFlowDbContext dbContext, IPasswordEncripter passwordEncripter)
+        private User AddUsersTeamMember(
+            CashFlowDbContext dbContext, 
+            IPasswordEncripter passwordEncripter, 
+            IAccessTokenGenerator accessTokenGenerator)
         {
             // Criando um usuário de teste usando o UserBuilder e adicionando-o ao banco de dados em memória
-            _user = UserBuilder.Build();// Armazenando a senha original para uso nos testes
-            _password = _user.Password;
+            var user = UserBuilder.Build();// Armazenando a senha original para uso nos testes
+            var password = user.Password;
 
-            _user.Password = passwordEncripter.Encrypt(_user.Password); // Encriptando a senha do usuário antes de adicioná-lo ao banco de dados
+            user.Password = passwordEncripter.Encrypt(user.Password); // Encriptando a senha do usuário antes de adicioná-lo ao banco de dados
 
-            dbContext.Users.Add(_user);
+            dbContext.Users.Add(user);
+
+            var token = accessTokenGenerator.Generate(user); // Gerando um token de acesso para o usuário de teste criado no banco de dados em memória
+
+            // Criando uma instância de UserIdentityManager para o usuário de teste, que pode ser usada nos testes para obter informações sobre o usuário, como nome, email, senha e token de acesso
+            User_Team_Member = new UserIdentityManager(user, password, token);
+
+            return user;
         }
 
         private void AddExpenses(CashFlowDbContext dbContext, User user)
         {
-            _expense = ExpenseBuilder.Build(user);
+            var expense = ExpenseBuilder.Build(user);
 
-            dbContext.Expenses.Add(_expense);
+            dbContext.Expenses.Add(expense);
+
+            Expense = new ExpenseIdentityManager(expense);
         }
     }
 }
