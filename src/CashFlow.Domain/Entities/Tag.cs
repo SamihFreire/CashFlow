e2d2a7d83c3904c57b214c2a6623eb6aa984033b
SamihFreire <samihcisne@gmail.com>
2026-02-19 14:52:27 -0300

    feat: adicionado tags para despesas

---
+++ src/CashFlow.Domain/Entities/Tag.cs
@@ -0,0 +1,11 @@
+﻿namespace CashFlow.Domain.Entities
+{
+    public class Tag
+    {
+        public long Id { get; set; }
+        public Enums.Tag Value { get; set; }
+
+        public long ExpenseId { get; set; }
+        public Expense Expense { get; set; } = default!;
+    }
+}
